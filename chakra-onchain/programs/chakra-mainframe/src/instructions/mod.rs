e476422785cf139c78cbcff5bbdc2fdef4d6c409
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2026-04-07 22:17:28 +0530

    feat: regirster admit inst module

--- chakra-onchain/programs/chakra-mainframe/src/instructions/mod.rs
+++ chakra-onchain/programs/chakra-mainframe/src/instructions/mod.rs
@@ -1,7 +1,9 @@
 pub mod initialize_intent;
 pub mod cancel_intent;
 pub mod submit_proof;
+pub mod admin;
 
 pub use initialize_intent::*;
 pub use cancel_intent::*;
 pub use submit_proof::*;
+pub use admin::*;
