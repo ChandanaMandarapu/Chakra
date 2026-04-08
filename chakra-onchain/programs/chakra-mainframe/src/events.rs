7a55f79346c8ce1555ec5fa3f89f79038de6faeb
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2026-04-08 20:48:57 +0530

    refactor: transition protocol events to byte arrays for stack-efficient serialization

--- chakra-onchain/programs/chakra-mainframe/src/events.rs
+++ chakra-onchain/programs/chakra-mainframe/src/events.rs
@@ -4,9 +4,9 @@ use anchor_lang::prelude::*;
 pub struct ControlIntent {
     pub owner: Pubkey,
     pub amount: u64,
-    pub source_chain: String,
-    pub target_chain: String,
-    pub target_address: String,
+    pub source_chain: [u8; 32],
+    pub target_chain: [u8; 32],
+    pub target_address: [u8; 64],
     pub escrow_pda: Pubkey,
     pub timeout_slot: u64,
 }
@@ -14,7 +14,7 @@ pub struct ControlIntent {
 #[event]
 pub struct IntentFinalized {
     pub escrow_pda: Pubkey,
-    pub tx_hash: String,
+    pub tx_hash: [u8; 64],
 }
 
 #[event]
