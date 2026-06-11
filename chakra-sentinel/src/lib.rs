3fb89ec6907c0e8c828d814db8b62d4e0fe8bb96
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2026-06-11 14:00:00 +0530

    refactor: register errors module in sentinel main library module

--- chakra-sentinel/src/lib.rs
+++ chakra-sentinel/src/lib.rs
@@ -2,11 +2,12 @@ pub mod listener;
 pub mod processor;
 pub mod signer;
 pub mod state;
-
 pub mod node_server;
+pub mod errors;
 
 pub use listener::*;
 pub use processor::*;
 pub use signer::*;
 pub use state::*;
 pub use node_server::*;
+pub use errors::*;
